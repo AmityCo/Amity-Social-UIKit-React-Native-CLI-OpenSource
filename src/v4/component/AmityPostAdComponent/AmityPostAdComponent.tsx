4bd0e9e42b8b5042b37722468cf8609aa24ecd58
ptchaya_p <ptchaya.p@gmail.com>
2025-03-17 17:35:09 +0700

    feat: add other post ad components

--- src/v4/component/AmityPostAdComponent/AmityPostAdComponent.tsx
+++ src/v4/component/AmityPostAdComponent/AmityPostAdComponent.tsx
@@ -1,11 +1,13 @@
-import React, { FC, memo } from 'react';
-import { View } from 'react-native';
-import { SvgXml } from 'react-native-svg';
-import { infoIcon } from '../../../svg/svg-xml-list';
+import React, { FC, memo, useEffect, useState } from 'react';
+import { Image, View, TouchableOpacity, Linking } from 'react-native';
+
 import { ComponentID, PageID } from '../../enum';
 import { useStyles } from './styles';
 import PostAdHeader from './PostAdHeader';
-import { useAmityComponent } from '../../hook';
+import { useAmityComponent, useFile } from '../../hook';
+import { Text } from 'react-native-paper';
+import { infoIcon } from '../../../svg/svg-xml-list';
+import { SvgXml } from 'react-native-svg';
 
 type AmityPostAdComponentType = {
   pageId?: PageID;
@@ -14,18 +16,52 @@ type AmityPostAdComponentType = {
 
 const AmityPostAdComponent: FC<AmityPostAdComponentType> = ({ ad, pageId }) => {
   const componentId = ComponentID.post_content;
+  const [image, setImage] = useState<string | null>(null);
   const { accessibilityId, themeStyles } = useAmityComponent({
     pageId: pageId,
     componentId: componentId,
   });
+
+  const { getImage } = useFile();
+
   const styles = useStyles(themeStyles);
+
+  useEffect(() => {
+    const fetchImage = async (fileId: string) => {
+      const result = await getImage({ fileId });
+      setImage(result);
+    };
+
+    if (!ad?.image1_1.fileId) return;
+    fetchImage(ad?.image1_1.fileId);
+  }, [ad?.image1_1.fileId, getImage]);
 
   if (!ad) return null;
 
   return (
     <View style={styles.container} testID={accessibilityId}>
-      <SvgXml xml={infoIcon()} width="16" height="16" />
+      <SvgXml style={styles.infoIcon} xml={infoIcon()} width="16" height="16" />
       <PostAdHeader advertiser={ad?.advertiser} pageId={pageId} />
+      <Text style={styles.textContent}>{ad.body}</Text>
+      {image && (
+        <Image
+          source={{ uri: image }}
+          style={styles.image}
+          resizeMode="cover"
+        />
+      )}
+      <View style={styles.footer}>
+        <View style={styles.footerTextWrap}>
+          <Text style={styles.footerDescription}>{ad?.description}</Text>
+          <Text style={styles.footerHeadline}>{ad?.headline}</Text>
+        </View>
+        <TouchableOpacity
+          style={styles.callToActionButton}
+          onPress={() => Linking.openURL(ad?.callToActionUrl)}
+        >
+          <Text style={styles.callToActionText}>{ad.callToAction}</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
