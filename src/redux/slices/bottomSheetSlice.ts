d26bd0ba9bb9a62fe4670d416430a53310ca483d
ptchaya_p <ptchaya.p@gmail.com>
2025-03-18 12:10:57 +0700

    feat: add bottom sheet slice

--- src/redux/slices/bottomSheetSlice.ts
+++ src/redux/slices/bottomSheetSlice.ts
@@ -1,33 +1,22 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
-import { ComponentID, PageID } from '../../v4/enum';
 
-export interface BottomSheetState {
+interface BottomSheetState {
   isBottomSheetOpen: boolean;
   content: JSX.Element | null;
-  height?: number;
-  pageId?: PageID;
-  componentId?: ComponentID;
 }
 
 const initialState: BottomSheetState = {
   isBottomSheetOpen: false,
   content: null,
-  height: 200,
-  pageId: PageID.WildCardPage,
-  componentId: ComponentID.WildCardComponent,
 };
 
 const bottomSheetSlice = createSlice({
   name: 'bottomSheet',
   initialState,
   reducers: {
-    openBottomSheet: (
-      state,
-      action: PayloadAction<{ content: JSX.Element; height?: number }>
-    ) => {
+    openBottomSheet: (state, action: PayloadAction<JSX.Element>) => {
       state.isBottomSheetOpen = true;
-      state.content = action.payload.content;
-      state.height = action.payload.height || 200;
+      state.content = action.payload;
     },
     closeBottomSheet: (state) => {
       state.isBottomSheetOpen = false;
