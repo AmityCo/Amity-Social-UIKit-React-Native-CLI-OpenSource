ac8b9927ee1271d787446a63e5695ff930aa810c
ptchaya_p <ptchaya.p@gmail.com>
2025-03-18 10:54:32 +0700

    feat: add bottom sheet ui

--- src/v4/component/AmityPostAdComponent/styles.ts
+++ src/v4/component/AmityPostAdComponent/styles.ts
@@ -102,6 +102,55 @@ export const useStyles = (theme: MyMD3Theme) => {
       color: 'white',
       lineHeight: 18,
     },
+    bottomSheet: {
+      borderTopLeftRadius: 20,
+      borderTopRightRadius: 20,
+      backgroundColor: theme.colors.background,
+      width: '100%',
+    },
+    bottomSheetHeader: {
+      fontSize: 17,
+      lineHeight: 22,
+      textAlign: 'center',
+      color: theme.colors.base,
+      fontWeight: '600',
+      paddingVertical: 13,
+    },
+    divider: {
+      height: 1,
+      backgroundColor: theme.colors.baseShade4,
+    },
+    bottomSheetContent: {
+      paddingHorizontal: 16,
+    },
+    buttomSheetContentItem: {
+      marginTop: 24,
+      marginBottom: 8,
+    },
+    buttomSheetContentTitle: {
+      fontWeight: '600',
+      fontSize: 15,
+      lineHeight: 20,
+      color: theme.colors.base,
+      marginBottom: 8,
+    },
+    buttomSheetContentDetail: {
+      flexDirection: 'row',
+      padding: 8,
+      gap: 8,
+      backgroundColor: theme.colors.baseShade4,
+      borderRadius: 8,
+    },
+    buttomSheetContentDetailIcon: {
+      width: 16,
+      height: 16,
+      color: theme.colors.baseShade1,
+    },
+    buttomSheetContentDetailText: {
+      fontSize: 13,
+      lineHeight: 18,
+      color: theme.colors.baseShade1,
+    },
   });
 
   return styles;
