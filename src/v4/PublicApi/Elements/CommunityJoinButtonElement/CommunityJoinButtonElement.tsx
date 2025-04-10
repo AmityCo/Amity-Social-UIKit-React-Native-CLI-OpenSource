b82bf43ad180c24b89e426582059b6585f73f7b8
ptchaya_p <ptchaya.p@gmail.com>
2025-04-10 15:42:01 +0700

    feat: add join and joined button

---
+++ src/v4/PublicApi/Elements/CommunityJoinButtonElement/CommunityJoinButtonElement.tsx
@@ -0,0 +1,58 @@
+import { Image } from 'react-native';
+import React, { FC, memo } from 'react';
+import {
+  ComponentID,
+  ElementID,
+  PageID,
+  UiKitConfigKeys,
+} from '../../../enum/enumUIKitID';
+import useConfig from '../../../hook/useConfig';
+import { useAmityElement, useConfigImageUri } from '../../../hook';
+import { Button } from '../../../component/Button/Button';
+
+type CommunityJoinButtonElementType = {
+  pageId?: PageID;
+  componentId?: ComponentID;
+};
+
+const CommunityJoinButtonElement: FC<CommunityJoinButtonElementType> = ({
+  pageId = PageID.WildCardPage,
+  componentId = ComponentID.WildCardComponent,
+  ...props
+}) => {
+  const { excludes } = useConfig();
+
+  const elementId = ElementID.community_join_button;
+  const configKey: keyof UiKitConfigKeys = 'icon';
+  const configId = `${pageId}/${componentId}/${elementId}`;
+
+  const { config, accessibilityId } = useAmityElement({
+    pageId,
+    componentId,
+    elementId,
+  });
+
+  const imageSource = useConfigImageUri({
+    configPath: {
+      page: pageId,
+      component: componentId,
+      element: elementId,
+    },
+    configKey: configKey,
+  });
+
+  if (excludes.includes(configId)) return null;
+
+  return (
+    <Button
+      testID={accessibilityId}
+      type="primary"
+      icon={<Image source={imageSource} />}
+      {...props}
+    >
+      {config.text as string}
+    </Button>
+  );
+};
+
+export default memo(CommunityJoinButtonElement);
