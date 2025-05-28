f70cf664615c50ae4c71bc85bcbcc208fb5f8f58
topAmity <112688936+topAmity@users.noreply.github.com>
2025-05-28 23:40:52 +0700

    add export screen function

--- src/v4/screen/PostDetail/index.tsx
+++ src/v4/screen/PostDetail/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import AmityPostDetailPage from '../../PublicApi/Pages/AmityPostDetailPage/AmityPostDetailPage';
-import { RouteProp } from '@react-navigation/native';
+import { RouteProp, useRoute } from '@react-navigation/native';
 import { RootStackParamList } from '../../../routes/RouteParamList';
 
 type IPostDetailPage = {
-  route: RouteProp<RootStackParamList, 'PostDetail'>;
+  defaultPostId?: string;
 };
 
-const PostDetail: React.FC<IPostDetailPage> = ({ route }) => {
-  const { postId } = route.params;
-  return <AmityPostDetailPage postId={postId} />;
+const PostDetail: React.FC<IPostDetailPage> = ({ defaultPostId }) => {
+  const route = useRoute<RouteProp<RootStackParamList, 'PostDetail'>>();
+  const postIdFromRoute = route?.params?.postId;
+
+  console.log('postId: ', postIdFromRoute);
+
+  return (
+    <AmityPostDetailPage postId={defaultPostId ?? postIdFromRoute} />
+  );
 };
 
 export default PostDetail;
