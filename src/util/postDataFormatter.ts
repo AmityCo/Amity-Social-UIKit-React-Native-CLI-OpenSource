63f8c33c2c01f8316eba9b31af65719d78471b8d
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2025-06-24 16:23:59 +0700

    fix: remove comments

--- src/util/postDataFormatter.ts
+++ src/util/postDataFormatter.ts
@@ -5,23 +5,6 @@ export const amityPostsFormatter = async (
     posts.map(async (item: Amity.Post<any>) => {
       return {
         ...item,
-        // postId: item.postId,
-        // data: item.data as Record<string, any>,
-        // dataType: item?.dataType ?? 'text',
-        // myReactions: item.myReactions as string[],
-        // reactionCount: item.reactions as Record<string, number>,
-        // commentsCount: item.commentsCount,
-        // user: userObject.data as UserInterface,
-        // editedAt: item.editedAt,
-        // createdAt: item.createdAt,
-        // updatedAt: item.updatedAt,
-        // targetType: item.targetType,
-        // targetId: item.targetId,
-        // childrenPosts: item.children,
-        // mentionees: item.mentionees[0]?.userIds,
-        // mentionPosition: item?.metadata?.mentioned || undefined,
-        // path: item.path,
-        // analytics: item.analytics,
       };
     })
   );
