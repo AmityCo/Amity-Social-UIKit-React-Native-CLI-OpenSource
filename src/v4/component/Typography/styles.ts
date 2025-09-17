9fa1c3e045244c6eb14da9b2948dd08ab4b8a142
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2025-09-17 16:32:56 +0700

    feat: typography default color

--- src/v4/component/Typography/styles.ts
+++ src/v4/component/Typography/styles.ts
@@ -1,44 +1,59 @@
 import { StyleSheet } from 'react-native';
+import { useTheme } from 'react-native-paper';
+import { MyMD3Theme } from '~/providers/amity-ui-kit-provider';
 
-export const styles = StyleSheet.create({
-  typography__headline: {
-    fontSize: 20,
-    fontWeight: '700',
-    lineHeight: 24,
-  },
-  typography__titleBold: {
-    fontSize: 17,
-    fontWeight: '600',
-    lineHeight: 24,
-  },
-  typography__title: {
-    fontSize: 17,
-    fontWeight: '400',
-    lineHeight: 24,
-  },
-  typography__bodyBold: {
-    fontSize: 15,
-    fontWeight: '600',
-    lineHeight: 20,
-  },
-  typography__body: {
-    fontSize: 15,
-    fontWeight: '400',
-    lineHeight: 20,
-  },
-  typography__captionBold: {
-    fontSize: 13,
-    fontWeight: '600',
-    lineHeight: 18,
-  },
-  typography__caption: {
-    fontSize: 13,
-    fontWeight: '400',
-    lineHeight: 18,
-  },
-  typography__captionSmall: {
-    fontSize: 10,
-    fontWeight: '400',
-    lineHeight: 13,
-  },
-});
+export const useStyles = () => {
+  const theme = useTheme<MyMD3Theme>();
+  const styles = StyleSheet.create({
+    typography__headline: {
+      fontSize: 20,
+      fontWeight: '700',
+      lineHeight: 24,
+      color: theme.colors.base,
+    },
+    typography__titleBold: {
+      fontSize: 17,
+      fontWeight: '600',
+      lineHeight: 24,
+      color: theme.colors.base,
+    },
+    typography__title: {
+      fontSize: 17,
+      fontWeight: '400',
+      lineHeight: 24,
+      color: theme.colors.base,
+    },
+    typography__bodyBold: {
+      fontSize: 15,
+      fontWeight: '600',
+      lineHeight: 20,
+      color: theme.colors.base,
+    },
+    typography__body: {
+      fontSize: 15,
+      fontWeight: '400',
+      lineHeight: 20,
+      color: theme.colors.base,
+    },
+    typography__captionBold: {
+      fontSize: 13,
+      fontWeight: '600',
+      lineHeight: 18,
+      color: theme.colors.base,
+    },
+    typography__caption: {
+      fontSize: 13,
+      fontWeight: '400',
+      lineHeight: 18,
+      color: theme.colors.base,
+    },
+    typography__captionSmall: {
+      fontSize: 10,
+      fontWeight: '400',
+      lineHeight: 13,
+      color: theme.colors.base,
+    },
+  });
+
+  return { styles };
+};
