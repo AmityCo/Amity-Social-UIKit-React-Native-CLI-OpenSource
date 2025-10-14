121c19c692f877ce18c35d57938c26899977fa51
ChayanitBm <chayanit@amity.co>
2025-10-14 14:39:17 +0700

    fix: handle post error

--- src/v4/utils/errors.ts
+++ src/v4/utils/errors.ts
@@ -1,4 +1,8 @@
-import { COMMENT_ERROR_MESSAGE, ERROR_CODE } from '../constants';
+import {
+  COMMENT_ERROR_MESSAGE,
+  ERROR_CODE,
+  POST_ERROR_MESSAGE,
+} from '../constants';
 
 export const getCommentErrorMessage = (error: Error): string => {
   if (error.message.includes(ERROR_CODE.BLOCKED_WORD)) {
@@ -10,4 +14,21 @@ export const getCommentErrorMessage = (error: Error): string => {
   }
 
   return COMMENT_ERROR_MESSAGE.GENERIC;
+};
+
+export const getPostErrorMessage = (
+  error: Error,
+  isEditMode?: boolean
+): string => {
+  if (error.message.includes(ERROR_CODE.BLOCKED_WORD)) {
+    return POST_ERROR_MESSAGE.BLOCKED_WORD;
+  }
+
+  if (error.message.includes(ERROR_CODE.BLOCKED_URL)) {
+    return POST_ERROR_MESSAGE.BLOCKED_URL;
+  }
+
+  return isEditMode
+    ? POST_ERROR_MESSAGE.GENERIC_EDIT
+    : POST_ERROR_MESSAGE.GENERIC_CREATE;
 };
