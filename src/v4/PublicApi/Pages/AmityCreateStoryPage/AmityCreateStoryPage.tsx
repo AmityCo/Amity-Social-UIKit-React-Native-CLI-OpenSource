9cf36583dc75c0ebda81dc85297fbf97e50a9769
ptchaya_p <ptchaya.p@gmail.com>
2025-12-09 14:37:10 +0700

    fix: camera still black after grant the permission

--- src/v4/PublicApi/Pages/AmityCreateStoryPage/AmityCreateStoryPage.tsx
+++ src/v4/PublicApi/Pages/AmityCreateStoryPage/AmityCreateStoryPage.tsx
@@ -24,6 +24,8 @@ import {
   PhotoFile,
   VideoFile,
   useCameraFormat,
+  useCameraPermission,
+  useMicrophonePermission,
 } from 'react-native-vision-camera';
 import { useStyles } from './styles';
 import { SvgXml } from 'react-native-svg';
@@ -63,6 +65,11 @@ const AmityCreateStoryPage: FC<ICreateStoryPage> = ({
   onCreateStory,
   onDiscardStory,
 }) => {
+  const { hasPermission: hasCameraPermission } = useCameraPermission();
+  const { hasPermission: hasMicrophonePermission } = useMicrophonePermission();
+
+  const [cameraKey, setCameraKey] = useState(0);
+
   const { width, height } = useWindowDimensions();
   const isPortrait = width < height;
   const landscapeStyle: ViewStyle = !isPortrait && {
@@ -113,6 +120,19 @@ const AmityCreateStoryPage: FC<ICreateStoryPage> = ({
     clearInterval(timerRef?.current);
     cameraRef?.current?.stopRecording();
   }, []);
+
+  useEffect(() => {
+    if (backCamera && !activeCamera) {
+      setActiveCamera(backCamera);
+    }
+  }, [backCamera, activeCamera]);
+
+  useEffect(() => {
+    if (hasCameraPermission && hasMicrophonePermission && activeCamera) {
+      // Force remount camera component
+      setCameraKey((prev) => prev + 1);
+    }
+  }, [hasCameraPermission, hasMicrophonePermission, activeCamera]);
 
   useEffect(() => {
     if (totalTime === TIMER_LIMIT) return onStopRecord();
@@ -262,6 +282,7 @@ const AmityCreateStoryPage: FC<ICreateStoryPage> = ({
       <View style={styles.cameraContainer}>
         <Camera
           ref={cameraRef}
+          key={cameraKey}
           style={[styles.camera, landscapeStyle]}
           device={activeCamera}
           isActive={true}
