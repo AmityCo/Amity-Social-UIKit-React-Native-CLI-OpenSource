1fa7ca8514c8f5f3fe4a3a939c7e2d3224d42f2b
topAmity <112688936+topAmity@users.noreply.github.com>
2025-12-23 23:01:42 +0700

    remove sample code in example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,19 +1,12 @@
 import * as React from 'react';
 import {
-  AmityGlobalFeedComponent,
-  AmityGlobalStoryTabWrapper,
-  AmityPageRenderer,
-  AmityPostTargetSelectionPage,
-  AmityPostTargetSelectionPageType,
-  AmityStoryTabComponent,
-  AmityStoryTabComponentEnum,
   AmityUiKitProvider,
+  AmityUiKitSocial,
 } from '@amityco/react-native-social-uikit';
 import config from '../uikit.config.json';
 import messaging from '@react-native-firebase/messaging';
 import { useEffect, useState } from 'react';
-import { PermissionsAndroid, Platform, View } from 'react-native';
-import { Button } from 'react-native-paper';
+import { PermissionsAndroid, Platform } from 'react-native';
 
 messaging().setBackgroundMessageHandler(async (remoteMessage) => {
   console.log('Background notification:', remoteMessage);
@@ -104,68 +97,19 @@ export default function App() {
 
     return () => unsubscribe?.();
   }, [permissionGranted]);
-  const [showPostModal, setShowPostModal] = useState(false);
 
   if (!fcmToken) return null;
   return (
     <AmityUiKitProvider
       configs={config} //put your config json object
-      apiKey="b3babb0b3a89f4341d31dc1a01091edcd70f8de7b23d697f"
-      apiRegion="sg"
-      userId="topAmity"
-      displayName="topAmity"
-      apiEndpoint="https://api.sg.amity.co"
+      apiKey="YOUR_API_KEY" // Put your apiKey
+      apiRegion="API_REGION" // Put your apiRegion
+      userId="USER_ID" // Put your UserId
+      displayName="DISPLAYNAME" // Put your displayName
+      apiEndpoint="API_ENDPOINT" //"https://api.{apiRegion}.amity.co"
       fcmToken={fcmToken} // android:fcm iOS:APN
-      behaviour={{
-        AmityPostTargetSelectionPageBehavior: {
-          onClickClose: () => setShowPostModal(false),
-        },
-        AmityPostComposerPageBehavior: {
-          onPressPost: () => setShowPostModal(false),
-        },
-      }}
     >
-      <AmityPageRenderer>
-        <AmityGlobalFeedComponent
-          GlobalFeedHeaderComponent={
-            <View>
-              <AmityGlobalStoryTabWrapper>
-                <AmityStoryTabComponent
-                  type={AmityStoryTabComponentEnum.globalFeed}
-                />
-              </AmityGlobalStoryTabWrapper>
-              <View style={{ height: 200, backgroundColor: 'lightgray' }} />
-            </View>
-          }
-          isShowStoryTab={false}
-        />
-      </AmityPageRenderer>
-
-      {showPostModal && (
-        <View
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            backgroundColor: 'white',
-            zIndex: 10,
-          }}
-        >
-          <AmityPageRenderer>
-            <AmityPostTargetSelectionPage
-              postType={AmityPostTargetSelectionPageType.post}
-            />
-          </AmityPageRenderer>
-        </View>
-      )}
-      {/* <AmityUiKitSocial/> */}
-      <View style={{ height: 80, backgroundColor: 'lightgray' }}>
-        <Button onPress={() => setShowPostModal(true)}>
-          Open Post Target Selection
-        </Button>
-      </View>
+      <AmityUiKitSocial />
     </AmityUiKitProvider>
   );
 }
