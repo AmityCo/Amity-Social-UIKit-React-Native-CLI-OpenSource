22a8f9f7fa1520bacf04659ca3cf3aa426e8f267
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2026-01-23 14:00:19 +0700

    feat: pull latest

--- src/v4/PublicApi/Components/AmityCreatePostMenuComponent/AmityCreatePostMenuComponent.tsx
+++ src/v4/PublicApi/Components/AmityCreatePostMenuComponent/AmityCreatePostMenuComponent.tsx
@@ -8,7 +8,7 @@ import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../../../../v4/routes/RouteParamList';
 import { AmityPostTargetSelectionPageType } from '../../../enum';
 import { livestream, poll, post, story } from '~/v4/assets/icons';
-import MenuAction from '~/v4/elements/MenuAction';
+import MenuAction from '../../../../v4/elements/MenuAction';
 
 interface AmityCreatePostMenuComponentProps {
   pageId?: PageID;
