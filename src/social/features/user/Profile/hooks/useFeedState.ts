82c5e5865df984e81229a095f30987bd81fe5652
ChayanitBm <chayanit@amity.co>
2026-05-18 11:47:52 +0700

    fix: showing privaye UI

--- src/social/features/user/Profile/hooks/useFeedState.ts
+++ src/social/features/user/Profile/hooks/useFeedState.ts
@@ -1,6 +1,6 @@
 import useAuth from '../../../../../core/hooks/useAuth';
 import useSocialSettings from '../../../../../core/hooks/useSocialSettings';
-import { useFollowInfo } from '../../../../hooks/objects';
+import { useFollowInfo, useUser } from '../../../../hooks/objects';
 
 type UseFeedStateParams = {
   userId: string;
@@ -10,13 +10,15 @@ export function useFeedState({ userId }: UseFeedStateParams) {
   const { client } = useAuth();
   const { socialSettings } = useSocialSettings();
   const { followInfo } = useFollowInfo({ userId, enabled: !!userId });
+  const { user } = useUser({ userId });
 
   const isMyProfile = client?.userId === userId;
   const isBlockedByMe = followInfo?.status === 'blocked';
   const isFollowing = followInfo?.status === 'accepted';
+  const isBrandUser = !!user?.isBrand;
   const isPrivate =
     !isMyProfile &&
-    socialSettings?.userPrivacySetting === 'private' &&
+    (isBrandUser || socialSettings?.userPrivacySetting === 'private') &&
     !isFollowing;
 
   return {
