7e3aa58db6ff5b050da769017548f5ce6e5b0142
ChayanitBm <chayanit@amity.co>
2026-05-29 18:32:22 +0700

    fix: moderator check

--- src/v4/hook/useStoryPermission.ts
+++ src/v4/hook/useStoryPermission.ts
@@ -1,26 +1,25 @@
 import useAuth from '../../hooks/useAuth';
 import { useUser } from './useUser';
 import useSocialSettings from '../core/hooks/useSocialSettings';
-import {
-  isAdmin,
-  isModerator,
-  checkStoryPermission,
-} from '../utils/permissions';
+import { isAdmin } from '../utils/permissions';
+import { useIsCommunityModerator } from './useIsCommunityModerator';
 
 export function useStoryPermission(communityId?: string) {
   const { client } = useAuth();
   const { socialSettings } = useSocialSettings();
   const user = useUser(client?.userId || '');
+  const { isCommunityModerator } = useIsCommunityModerator({
+    userId: client?.userId || '',
+    communityId: communityId || '',
+  });
 
   const isGlobalAdmin = isAdmin(user?.roles);
-  const isModeratorUser = isModerator(user?.roles);
 
   const hasStoryPermission = !communityId
     ? socialSettings?.story?.allowAllUserToCreateStory
     : socialSettings?.story?.allowAllUserToCreateStory ||
       isGlobalAdmin ||
-      isModeratorUser ||
-      checkStoryPermission(client, communityId);
+      isCommunityModerator;
 
   return hasStoryPermission;
 }
