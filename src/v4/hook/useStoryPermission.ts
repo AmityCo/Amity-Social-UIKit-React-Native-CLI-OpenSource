6ffbfbaf3721fbb0cebc98237833a4a28ecf6612
ChayanitBm <chayanit@amity.co>
2026-06-04 15:27:11 +0700

    fix: remove moderator check

--- src/v4/hook/useStoryPermission.ts
+++ src/v4/hook/useStoryPermission.ts
@@ -1,11 +1,7 @@
 import useAuth from '../../hooks/useAuth';
 import { useUser } from './useUser';
 import useSocialSettings from '../core/hooks/useSocialSettings';
-import {
-  checkStoryPermission,
-  isAdmin,
-  isModerator,
-} from '../utils/permissions';
+import { checkStoryPermission, isAdmin } from '../utils/permissions';
 
 export function useStoryPermission(communityId?: string) {
   const { client } = useAuth();
@@ -13,13 +9,11 @@ export function useStoryPermission(communityId?: string) {
   const user = useUser(client?.userId || '');
 
   const isGlobalAdmin = isAdmin(user?.roles);
-  const isModeratorUser = isModerator(user?.roles);
 
   const hasStoryPermission = !communityId
     ? socialSettings?.story?.allowAllUserToCreateStory
     : socialSettings?.story?.allowAllUserToCreateStory ||
       isGlobalAdmin ||
-      isModeratorUser ||
       checkStoryPermission(client, communityId);
 
   return { hasStoryPermission };
